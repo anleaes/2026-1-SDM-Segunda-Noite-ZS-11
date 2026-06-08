46c6507bf5a6e04864140062d477ea95e390ba10
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:07:56 -0300

    Documenta serializer de persons

--- gestao-contratos-backend/apps/persons/serializer.py
+++ gestao-contratos-backend/apps/persons/serializer.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
+﻿from rest_framework import serializers
 from .models import Person
 
 class PersonSerializer(serializers.ModelSerializer):
+    """Serializa os dados de persons para a API."""
     class Meta:
         model = Person
         fields = '__all__'
