95dc24aa662e1fa17dd41065eff741fdc214e406
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:09:50 -0300

    Documenta serializer de contactchannels

--- gestao-contratos-backend/apps/contactchannels/serializer.py
+++ gestao-contratos-backend/apps/contactchannels/serializer.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
+﻿from rest_framework import serializers
 from .models import ContactChannel
 
 class ContactChannelSerializer(serializers.ModelSerializer):
+    """Serializa os dados de contactchannels para a API."""
     class Meta:
         model = ContactChannel
         fields = '__all__'
