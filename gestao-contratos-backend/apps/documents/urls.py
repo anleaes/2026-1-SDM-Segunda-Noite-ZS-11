410dae8950ad8faf556fb6b3dc7e185676320262
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:12:34 -0300

    Organiza router de documents

--- gestao-contratos-backend/apps/documents/urls.py
+++ gestao-contratos-backend/apps/documents/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, include
+﻿from django.urls import path, include
 from rest_framework import routers
 from . import views
 
 app_name = 'documents'
 
 router = routers.DefaultRouter()
+# Registra o ViewSet principal do app no router padrao.
 router.register('', views.DocumentViewSet, basename='documentos')
 
 urlpatterns = [
