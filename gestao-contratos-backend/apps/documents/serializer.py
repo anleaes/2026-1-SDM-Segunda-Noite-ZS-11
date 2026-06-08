03246e6f0a50c9fbfab895c07790bc3d3d6a2adc
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:12:41 -0300

    Documenta serializer de documents

--- gestao-contratos-backend/apps/documents/serializer.py
+++ gestao-contratos-backend/apps/documents/serializer.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
+﻿from rest_framework import serializers
 from .models import Document
 
 class DocumentSerializer(serializers.ModelSerializer):
+    """Serializa os dados de documents para a API."""
     class Meta:
         model = Document
         fields = '__all__'
