9dfb79578d3cec89951ec5714831c419bfe8699d
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:09:50 -0300

    Ajusta configuracao do serializer de contactchannels

--- gestao-contratos-backend/apps/contactchannels/serializer.py
+++ gestao-contratos-backend/apps/contactchannels/serializer.py
@@ -4,5 +4,6 @@ from .models import ContactChannel
 class ContactChannelSerializer(serializers.ModelSerializer):
     """Serializa os dados de contactchannels para a API."""
     class Meta:
+        """Configuracao principal do serializer de contactchannels."""
         model = ContactChannel
         fields = '__all__'
