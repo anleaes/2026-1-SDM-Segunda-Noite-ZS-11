10bc1ea3ac29ea7a09865655e450b832352b0e53
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:09:36 -0300

    Documenta viewset de contactchannels

--- gestao-contratos-backend/apps/contactchannels/views.py
+++ gestao-contratos-backend/apps/contactchannels/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+﻿from rest_framework import viewsets
 from .models import ContactChannel
 from .serializer import ContactChannelSerializer
 
 class ContactChannelViewSet(viewsets.ModelViewSet):
+    """Endpoint REST para operacoes de contactchannels."""
     queryset = ContactChannel.objects.all()
     serializer_class = ContactChannelSerializer
