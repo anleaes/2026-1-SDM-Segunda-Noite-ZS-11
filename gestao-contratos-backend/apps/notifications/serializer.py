c148d327a57ad4af32f5fb07636d887dfc81c2c5
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:13:10 -0300

    Ajusta configuracao do serializer de notifications

--- gestao-contratos-backend/apps/notifications/serializer.py
+++ gestao-contratos-backend/apps/notifications/serializer.py
@@ -4,5 +4,6 @@ from .models import Notification
 class NotificationSerializer(serializers.ModelSerializer):
     """Serializa os dados de notifications para a API."""
     class Meta:
+        """Configuracao principal do serializer de notifications."""
         model = Notification
         fields = '__all__'
