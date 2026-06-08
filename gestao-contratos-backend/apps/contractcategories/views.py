641cfe841675a0a0fbd86fc40ec6fca42ca2edad
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:10:04 -0300

    Documenta viewset de contractcategories

--- gestao-contratos-backend/apps/contractcategories/views.py
+++ gestao-contratos-backend/apps/contractcategories/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+﻿from rest_framework import viewsets
 from .models import ContractCategory
 from .serializer import ContractCategorySerializer
 
 class ContractCategoryViewSet(viewsets.ModelViewSet):
+    """Endpoint REST para operacoes de contractcategories."""
     queryset = ContractCategory.objects.all()
     serializer_class = ContractCategorySerializer
