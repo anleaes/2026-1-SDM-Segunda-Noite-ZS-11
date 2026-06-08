9fd9d2bb9e55f71cf5fb4dd904a2cb1869614153
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:08:53 -0300

    Documenta serializer de employees

--- gestao-contratos-backend/apps/employees/serializer.py
+++ gestao-contratos-backend/apps/employees/serializer.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
+﻿from rest_framework import serializers
 from .models import Employee
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    """Serializa os dados de employees para a API."""
     class Meta:
         model = Employee
         fields = '__all__'
