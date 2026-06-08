ecbd8e40724e70f730e645ccf025f398962c2dcb
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:11:30 -0300

    Documenta viewset de contractitems

--- gestao-contratos-backend/apps/contractitems/views.py
+++ gestao-contratos-backend/apps/contractitems/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+﻿from rest_framework import viewsets
 from .models import ContractItem
 from .serializer import ContractItemSerializer
 
 class ContractItemViewSet(viewsets.ModelViewSet):
+    """Endpoint REST para operacoes de contractitems."""
     queryset = ContractItem.objects.all()
     serializer_class = ContractItemSerializer
