3b70f5956d36b3be3de447cbc1dee76dccd5cff3
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:08:53 -0300

    Ajusta configuracao do serializer de employees

--- gestao-contratos-backend/apps/employees/serializer.py
+++ gestao-contratos-backend/apps/employees/serializer.py
@@ -4,5 +4,6 @@ from .models import Employee
 class EmployeeSerializer(serializers.ModelSerializer):
     """Serializa os dados de employees para a API."""
     class Meta:
+        """Configuracao principal do serializer de employees."""
         model = Employee
         fields = '__all__'
