2cbf63c5f85970bc7a2ab5b928bcd39388fbac90
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:07:42 -0300

    Documenta viewset de persons

--- gestao-contratos-backend/apps/persons/views.py
+++ gestao-contratos-backend/apps/persons/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+﻿from rest_framework import viewsets
 from .models import Person
 from .serializer import PersonSerializer
 
 class PersonViewSet(viewsets.ModelViewSet):
+    """Endpoint REST para operacoes de persons."""
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
