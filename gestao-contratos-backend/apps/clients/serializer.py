474bbd7514f541205d55cce905a1036d1b001944
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:08:24 -0300

    Documenta serializer de clients

--- gestao-contratos-backend/apps/clients/serializer.py
+++ gestao-contratos-backend/apps/clients/serializer.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
+﻿from rest_framework import serializers
 from .models import Client
 
 class ClientSerializer(serializers.ModelSerializer):
+    """Serializa os dados de clients para a API."""
     class Meta:
         model = Client
         fields = '__all__'
