a418d19430c817778f59137b0ca90e63961c73a5
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:12:13 -0300

    Documenta serializer de payments

--- gestao-contratos-backend/apps/payments/serializer.py
+++ gestao-contratos-backend/apps/payments/serializer.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
+﻿from rest_framework import serializers
 from .models import Payment
 
 class PaymentSerializer(serializers.ModelSerializer):
+    """Serializa os dados de payments para a API."""
     class Meta:
         model = Payment
         fields = '__all__'
