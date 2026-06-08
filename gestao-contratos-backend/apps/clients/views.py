0d6b8c0c3211547a46e302ea475cb03584dbd8c6
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:08:10 -0300

    Documenta viewset de clients

--- gestao-contratos-backend/apps/clients/views.py
+++ gestao-contratos-backend/apps/clients/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+﻿from rest_framework import viewsets
 from .models import Client
 from .serializer import ClientSerializer
 
 class ClientViewSet(viewsets.ModelViewSet):
+    """Endpoint REST para operacoes de clients."""
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
