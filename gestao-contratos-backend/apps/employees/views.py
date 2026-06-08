dfecdb01c0dc3029a4f52165a21365fd04316415
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:08:39 -0300

    Documenta viewset de employees

--- gestao-contratos-backend/apps/employees/views.py
+++ gestao-contratos-backend/apps/employees/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+﻿from rest_framework import viewsets
 from .models import Employee
 from .serializer import EmployeeSerializer
 
 class EmployeeViewSet(viewsets.ModelViewSet):
+    """Endpoint REST para operacoes de employees."""
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
