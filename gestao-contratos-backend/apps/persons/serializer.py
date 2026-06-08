fca79b30ca01fe5060f3438bae03f6504dd5012d
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:07:56 -0300

    Ajusta configuracao do serializer de persons

--- gestao-contratos-backend/apps/persons/serializer.py
+++ gestao-contratos-backend/apps/persons/serializer.py
@@ -4,5 +4,6 @@ from .models import Person
 class PersonSerializer(serializers.ModelSerializer):
     """Serializa os dados de persons para a API."""
     class Meta:
+        """Configuracao principal do serializer de persons."""
         model = Person
         fields = '__all__'
