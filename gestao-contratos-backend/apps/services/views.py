40ffe3eee2fae710127354518b3f616271539de2
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:10:32 -0300

    Documenta viewset de services

--- gestao-contratos-backend/apps/services/views.py
+++ gestao-contratos-backend/apps/services/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+﻿from rest_framework import viewsets
 from .models import Service
 from .serializer import ServiceSerializer
 
 class ServiceViewSet(viewsets.ModelViewSet):
+    """Endpoint REST para operacoes de services."""
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
