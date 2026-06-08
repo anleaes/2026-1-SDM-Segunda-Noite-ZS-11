e0c8ac44d45661df35d76ab54a696a9ba7a05ec0
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:07:35 -0300

    Documenta model de persons

--- gestao-contratos-backend/apps/persons/models.py
+++ gestao-contratos-backend/apps/persons/models.py
@@ -1,9 +1,10 @@
-from django.db import models
+﻿from django.db import models
 
 class Person(models.Model):
+    """Mantem os dados de persons alinhados ao dominio de contratos."""
     first_name = models.CharField('Primeiro nome', max_length=100)
     last_name = models.CharField('Sobrenome', max_length=100)
-    address = models.CharField('Endereço', max_length=150)
+    address = models.CharField('EndereÃ§o', max_length=150)
     phone = models.CharField('Telefone', max_length=30)
     email = models.EmailField('Email', max_length=100)
 
