0ee5b2ec6815330141e19a3ab25d161c93a773b1
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:10:18 -0300

    Documenta serializer de contractcategories

--- gestao-contratos-backend/apps/contractcategories/serializer.py
+++ gestao-contratos-backend/apps/contractcategories/serializer.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
+﻿from rest_framework import serializers
 from .models import ContractCategory
 
 class ContractCategorySerializer(serializers.ModelSerializer):
+    """Serializa os dados de contractcategories para a API."""
     class Meta:
         model = ContractCategory
         fields = '__all__'
