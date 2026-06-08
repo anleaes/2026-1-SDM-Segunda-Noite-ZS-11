bd12e7510a2db30824273a65d423b14d48bdff76
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:10:39 -0300

    Organiza router de services

--- gestao-contratos-backend/apps/services/urls.py
+++ gestao-contratos-backend/apps/services/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, include
+﻿from django.urls import path, include
 from rest_framework import routers
 from . import views
 
 app_name = 'services'
 
 router = routers.DefaultRouter()
+# Registra o ViewSet principal do app no router padrao.
 router.register('', views.ServiceViewSet, basename='servicos')
 
 urlpatterns = [
