8c51f26ec1603844b95626fc90c12b70cf4a4407
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:09:21 -0300

    Documenta serializer de useraccounts

--- gestao-contratos-backend/apps/useraccounts/serializer.py
+++ gestao-contratos-backend/apps/useraccounts/serializer.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
+﻿from rest_framework import serializers
 from .models import UserAccount
 
 class UserAccountSerializer(serializers.ModelSerializer):
+    """Serializa os dados de useraccounts para a API."""
     class Meta:
         model = UserAccount
         fields = '__all__'
