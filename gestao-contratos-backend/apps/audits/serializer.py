744db774222ec5e8a0484fb51e07327c7fe6316e
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:13:39 -0300

    Ajusta configuracao do serializer de audits

--- gestao-contratos-backend/apps/audits/serializer.py
+++ gestao-contratos-backend/apps/audits/serializer.py
@@ -4,5 +4,6 @@ from .models import Audit
 class AuditSerializer(serializers.ModelSerializer):
     """Serializa os dados de audits para a API."""
     class Meta:
+        """Configuracao principal do serializer de audits."""
         model = Audit
         fields = '__all__'
