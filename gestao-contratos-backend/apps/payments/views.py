5de1a0c05d12f2d37eec75c550d0cdf21f32e64f
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:11:58 -0300

    Documenta viewset de payments

--- gestao-contratos-backend/apps/payments/views.py
+++ gestao-contratos-backend/apps/payments/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+﻿from rest_framework import viewsets
 from .models import Payment
 from .serializer import PaymentSerializer
 
 class PaymentViewSet(viewsets.ModelViewSet):
+    """Endpoint REST para operacoes de payments."""
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
