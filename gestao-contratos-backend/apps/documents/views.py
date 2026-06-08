2a994dd2a8f645d840e7415e1d0793885d18bf6e
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:12:27 -0300

    Documenta viewset de documents

--- gestao-contratos-backend/apps/documents/views.py
+++ gestao-contratos-backend/apps/documents/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+﻿from rest_framework import viewsets
 from .models import Document
 from .serializer import DocumentSerializer
 
 class DocumentViewSet(viewsets.ModelViewSet):
+    """Endpoint REST para operacoes de documents."""
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
