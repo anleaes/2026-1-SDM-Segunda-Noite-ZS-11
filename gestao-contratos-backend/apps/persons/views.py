ab9bdf524b649e19c9d09ad56a6dd759a3a2e252
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:07:42 -0300

    Ajusta consulta base da view de persons

--- gestao-contratos-backend/apps/persons/views.py
+++ gestao-contratos-backend/apps/persons/views.py
@@ -4,5 +4,6 @@ from .serializer import PersonSerializer
 
 class PersonViewSet(viewsets.ModelViewSet):
     """Endpoint REST para operacoes de persons."""
+    # Mantem a consulta base explicita para o roteamento da API.
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
