202a62d88aa0f91d4edb9db486ab37e66aa2fc5b
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:13:39 -0300

    Documenta serializer de audits

--- gestao-contratos-backend/apps/audits/serializer.py
+++ gestao-contratos-backend/apps/audits/serializer.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
+﻿from rest_framework import serializers
 from .models import Audit
 
 class AuditSerializer(serializers.ModelSerializer):
+    """Serializa os dados de audits para a API."""
     class Meta:
         model = Audit
         fields = '__all__'
