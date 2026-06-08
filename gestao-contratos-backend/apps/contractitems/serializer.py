a09a3805cdb7942fa4e5ab3f4e4cfb88f55b9b8f
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:11:44 -0300

    Ajusta configuracao do serializer de contractitems

--- gestao-contratos-backend/apps/contractitems/serializer.py
+++ gestao-contratos-backend/apps/contractitems/serializer.py
@@ -4,5 +4,6 @@ from .models import ContractItem
 class ContractItemSerializer(serializers.ModelSerializer):
     """Serializa os dados de contractitems para a API."""
     class Meta:
+        """Configuracao principal do serializer de contractitems."""
         model = ContractItem
         fields = '__all__'
