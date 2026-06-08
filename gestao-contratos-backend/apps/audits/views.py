287c1e9dce03033015de51a41de1ed6671a78a87
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:13:25 -0300

    Documenta viewset de audits

--- gestao-contratos-backend/apps/audits/views.py
+++ gestao-contratos-backend/apps/audits/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+﻿from rest_framework import viewsets
 from .models import Audit
 from .serializer import AuditSerializer
 
 class AuditViewSet(viewsets.ModelViewSet):
+    """Endpoint REST para operacoes de audits."""
     queryset = Audit.objects.all()
     serializer_class = AuditSerializer
