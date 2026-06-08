a0fb557e19cf6aea4e652bcf009b9ee28790b910
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:13:10 -0300

    Documenta serializer de notifications

--- gestao-contratos-backend/apps/notifications/serializer.py
+++ gestao-contratos-backend/apps/notifications/serializer.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
+﻿from rest_framework import serializers
 from .models import Notification
 
 class NotificationSerializer(serializers.ModelSerializer):
+    """Serializa os dados de notifications para a API."""
     class Meta:
         model = Notification
         fields = '__all__'
