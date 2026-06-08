9ed7813caad29bca9eade99641a609b47244595a
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:12:42 -0300

    Ajusta configuracao do serializer de documents

--- gestao-contratos-backend/apps/documents/serializer.py
+++ gestao-contratos-backend/apps/documents/serializer.py
@@ -4,5 +4,6 @@ from .models import Document
 class DocumentSerializer(serializers.ModelSerializer):
     """Serializa os dados de documents para a API."""
     class Meta:
+        """Configuracao principal do serializer de documents."""
         model = Document
         fields = '__all__'
