deed811d5aadf0168c9dd7eb6d53fcb7a0557ed5
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:08:17 -0300

    Organiza router de clients

--- gestao-contratos-backend/apps/clients/urls.py
+++ gestao-contratos-backend/apps/clients/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, include
+﻿from django.urls import path, include
 from rest_framework import routers
 from . import views
 
 app_name = 'clients'
 
 router = routers.DefaultRouter()
+# Registra o ViewSet principal do app no router padrao.
 router.register('', views.ClientViewSet, basename='clientes')
 
 urlpatterns = [
