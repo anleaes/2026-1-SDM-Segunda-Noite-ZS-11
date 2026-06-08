73e36bb106c7eebe2ad3d7fff58d95df4693e59b
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:08:25 -0300

    Ajusta configuracao do serializer de clients

--- gestao-contratos-backend/apps/clients/serializer.py
+++ gestao-contratos-backend/apps/clients/serializer.py
@@ -4,5 +4,6 @@ from .models import Client
 class ClientSerializer(serializers.ModelSerializer):
     """Serializa os dados de clients para a API."""
     class Meta:
+        """Configuracao principal do serializer de clients."""
         model = Client
         fields = '__all__'
