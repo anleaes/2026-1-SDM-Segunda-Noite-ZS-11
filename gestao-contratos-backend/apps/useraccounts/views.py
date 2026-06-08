6e129fd27f7ac99a4b6c1afb22c6f0c42478630f
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:09:07 -0300

    Documenta viewset de useraccounts

--- gestao-contratos-backend/apps/useraccounts/views.py
+++ gestao-contratos-backend/apps/useraccounts/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+﻿from rest_framework import viewsets
 from .models import UserAccount
 from .serializer import UserAccountSerializer
 
 class UserAccountViewSet(viewsets.ModelViewSet):
+    """Endpoint REST para operacoes de useraccounts."""
     queryset = UserAccount.objects.all()
     serializer_class = UserAccountSerializer
