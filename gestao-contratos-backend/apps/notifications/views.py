1110e9db7939fc9dcdfffeee9a7000dee1a6604c
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:12:56 -0300

    Documenta viewset de notifications

--- gestao-contratos-backend/apps/notifications/views.py
+++ gestao-contratos-backend/apps/notifications/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
+﻿from rest_framework import viewsets
 from .models import Notification
 from .serializer import NotificationSerializer
 
 class NotificationViewSet(viewsets.ModelViewSet):
+    """Endpoint REST para operacoes de notifications."""
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
