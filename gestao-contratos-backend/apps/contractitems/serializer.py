b58e568decd0d83f7220cd60669f5f8dd006d86f
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:11:44 -0300

    Documenta serializer de contractitems

--- gestao-contratos-backend/apps/contractitems/serializer.py
+++ gestao-contratos-backend/apps/contractitems/serializer.py
@@ -1,7 +1,8 @@
-from rest_framework import serializers
+﻿from rest_framework import serializers
 from .models import ContractItem
 
 class ContractItemSerializer(serializers.ModelSerializer):
+    """Serializa os dados de contractitems para a API."""
     class Meta:
         model = ContractItem
         fields = '__all__'
