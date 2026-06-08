27393dfac3ea67bda2ed87f9e7f73b3d441cc4d6
Gabriel Mancio <mancio9@hotmail.com>
2026-06-07 21:08:32 -0300

    Documenta model de employees

--- gestao-contratos-backend/apps/employees/models.py
+++ gestao-contratos-backend/apps/employees/models.py
@@ -1,16 +1,17 @@
-from django.db import models
+﻿from django.db import models
 from persons.models import Person
 
 class Employee(Person):
-    registration = models.CharField('Matrícula', max_length=30, unique=True)
+    """Mantem os dados de employees alinhados ao dominio de contratos."""
+    registration = models.CharField('MatrÃ­cula', max_length=30, unique=True)
     position = models.CharField('Cargo', max_length=100)
     department = models.CharField('Departamento', max_length=100)
-    salary = models.DecimalField('Salário', max_digits=10, decimal_places=2, default=0)
+    salary = models.DecimalField('SalÃ¡rio', max_digits=10, decimal_places=2, default=0)
 
     class Meta:
         db_table = "gc_employee"
-        verbose_name = 'Funcionário'
-        verbose_name_plural = 'Funcionários'
+        verbose_name = 'FuncionÃ¡rio'
+        verbose_name_plural = 'FuncionÃ¡rios'
         ordering = ['id']
 
     def __str__(self):
